beb8fbf792051a88a7d8a58c3c5aeec3dc17a504
Kamil <slimak-75@wp.pl>
2021-05-09 12:38:16 +0200

    poprawy w formatowaniu daty

--- src/main/java/com/WebApplication3KW/controller/GreetingController.java
+++ src/main/java/com/WebApplication3KW/controller/GreetingController.java
@@ -1,18 +1,19 @@
 package com.WebApplication3KW.controller;
 
-import com.WebApplication3KW.dto.GreetingDTO;
-import com.WebApplication3KW.dto.MessageDTO;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.util.HtmlUtils;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Controller
 public class GreetingController {
     @MessageMapping("/chat")
     @SendTo("/topic/messages")
-    public GreetingDTO send(MessageDTO message) {
-        GreetingDTO greetingDTO = new GreetingDTO("Hello, " + HtmlUtils.htmlEscape(message.getText()));
-        return greetingDTO;
+    public String send() {
+        DateTimeFormatter europeanDateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
+        LocalDateTime localDateTime = LocalDateTime.now();
+        return localDateTime.format(europeanDateFormatter);
     }
 }
--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,36 +1,19 @@
 var stompClient = null;
 
-function setConnected(connected) {
-    document.getElementById('connect').disabled = connected;
-    document.getElementById('disconnect').disabled = !connected;
-    document.getElementById('conversationDiv').style.visibility
-        = connected ? 'visible' : 'hidden';
-    document.getElementById('response').innerHTML = '';
-}
-
 function connect() {
     var socket = new SockJS('/chat');
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function(frame) {
-        setConnected(true);
         console.log('Connected: ' + frame);
         stompClient.subscribe('/topic/messages', function(messageOutput) {
-            showMessageOutput(JSON.parse(messageOutput.body));
+            showMessageOutput(messageOutput.body);
         });
     });
 }
 
-function sendMessage() {
-    var text = document.getElementById('text').value;
-    stompClient.send("/app/chat", {},
-        JSON.stringify({'text':text}));
-}
-
 function showMessageOutput(messageOutput) {
     var response = document.getElementById('response');
-    var p = document.createElement('p');
-    p.style.wordWrap = 'break-word';
-    p.appendChild(document.createTextNode(messageOutput.message));
-    response.appendChild(p);
+    response.innerHTML = messageOutput.toString();
 }
 window.addEventListener("load", connect, false);
+setInterval(function(){ stompClient.send("/app/chat"); }, 5000);
